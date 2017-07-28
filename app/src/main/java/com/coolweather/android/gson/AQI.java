3e0c297168c244053cee92efd46ab948f59a3bdc
Dauntlesse <1612825060@qq.com>
2017-07-28 10:02:02 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by ASUS on 2017/7/27.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,21 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by ASUS on 2017/7/27.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
+
